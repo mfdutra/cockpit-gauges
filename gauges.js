0a8ea3d4ea22f5cd3ff61fbc8e1098f1b6ff1109
Marlon Dutra <mfdutra@gmail.com>
2011-09-18 19:29:34 -0300

    new: svg clock the gagues now are objects with methods to modify the markers

--- gauges.js
+++ gauges.js
@@ -17,7 +17,7 @@ function verticalGauge(p)
 	var box = paper.path('M 16 0 L 0 0 L 0 100 L 16 100');
 	box.attr({stroke: 'white'});
 
-	function drawArea(points, color)
+	var drawArea = function(points, color)
 	{
 		points[0] = 100 - points[0];
 		points[1] = 100 - points[1];
@@ -40,12 +40,25 @@ function verticalGauge(p)
 		for (var i = 0; i < p.red.length; i++)
 			drawArea(p.red[i], 'red');
 
-	// Marker limits
-	if (p.marker > 100) p.marker = 100;
-	if (p.marker < 0) p.marker = 0;
+	// Method to set marker position
+	this.marker = 0;
+	this.setMarker = function(markerPos)
+	{
+		// Marker limits
+		if (markerPos > 100) markerPos = 100;
+		if (markerPos < 0) markerPos = 0;
 
-	var marker = paper.path('M 4 ' + (100-p.marker) + ' l 12 -8 l 0 16 z');
-	marker.attr({fill: 'white'});
+		var t = markerPos - this.marker;
+		this.marker = markerPos;
+
+		markerArrow.animate({'translation': '0 ' + (t * -1)}, 1000, '>');
+	}
+
+	var markerArrow = paper.path('M 4 100 l 12 -8 l 0 16 z');
+	markerArrow.attr({fill: 'white'});
+
+	if (p.marker)
+		this.setMarker(p.marker);
 
 	box.toFront();
 }
@@ -94,13 +107,24 @@ function speedometer(p)
 	var outline = paper.path('M 11.135983719 77.785047453 a 52,52 0 1,1 97.728032562 0');
 	outline.attr({stroke: 'white'});
 
-	// Marker limits
-	if (p.marker > 100) p.marker = 100;
-	if (p.marker < 0) p.marker = 0;
+	var markerArrow = paper.path('M 57 60 L 60 10 L 63 60 z');
+	markerArrow.attr({fill: 'white', 'stroke-width': 0});
 
-	var marker = paper.path('M 57 60 L 60 10 L 63 60 z');
-	marker.attr({fill: 'white', 'stroke-width': 0});
-	marker.rotate(-110 + (p.marker * 2.2), 60, 60);
+	this.marker = 0;
+	this.setMarker = function(markerPos)
+	{
+		// Marker limits
+		if (markerPos > 100) markerPos = 100;
+		if (markerPos < 0) markerPos = 0;
+
+		// markerArrow.rotate(-110 + (p.marker * 2.2), 60, 60);
+		markerArrow.animate({'rotation': (-110 + (markerPos * 2.2)) + ' 60 60'}, 1000, '>');
+
+		this.marker = markerPos;
+	}
+
+	if (p.marker)
+		this.setMarker(p.marker);
 
 	var lineDots = paper.set();
 	lineDots.push(
@@ -112,15 +136,90 @@ function speedometer(p)
 	var pivot = paper.circle(60, 60, 5);
 	pivot.attr({fill: '#555', 'stroke-width': 0});
 
-	if (p.title)
+	this.setValue = function(valueStr)
 	{
-		var title = paper.text(60, 80, p.title);
-		title.attr({fill: 'white', 'font-size': '12px'});
+		value.attr({'text': valueStr});
 	}
+
+	this.setTitle = function(valueStr)
+	{
+		title.attr({'text': valueStr});
+	}
+
+	var value = paper.text(60, 95, '');
+	value.attr({fill: 'white', 'font-size': '16px'});
+
+	var title = paper.text(60, 80, '');
+	title.attr({fill: 'white', 'font-size': '12px'});
 
 	if (p.value)
 	{
-		var value = paper.text(60, 95, p.value);
-		value.attr({fill: 'white', 'font-size': '16px'});
+		this.setValue(p.value);
 	}
+
+	if (p.title)
+	{
+		this.setTitle(p.title);
+	}
+}
+
+function svgClock(x, y)
+{
+	var center = 80;
+	var paper = Raphael(x, y, 160, 160);
+
+	// Outer border
+	var border = paper.circle(center, center, 75);
+	border.attr({stroke: '#555', 'stroke-width': '6px'});
+
+	// 1-minute ticks
+	for (var angle=0; angle < 360; angle += 6)
+	{
+		var tick = paper.path('M ' + center + ' 11 v -3');
+		tick.attr({stroke: '#555', 'stroke-width': '1px'});
+		tick.rotate(angle, center, center);
+	}
+
+	// 5-minute ticks
+	for (var angle=0; angle < 360; angle += 30)
+	{
+		var tick = paper.path('M ' + center + ' 15 v -7');
+		tick.attr({stroke: '#555', 'stroke-width': '1px'});
+		tick.rotate(angle, center, center);
+	}
+
+	// clock pointers
+	var minute = paper.path('M 78.5 80 v -58 l 1.5 -4 l 1.5 4 v 58 z');
+	minute.attr({'stroke-width': 0, 'fill': 'white'});
+	var minuteL = paper.path('M 80 80 v -58');
+	minuteL.attr({'stroke-width': '1px', 'stroke': '#aaa'});
+
+	var hour = paper.path('M 78.5 80 v -38 l 1.5 -4 l 1.5 4 v 38 z');
+	hour.attr({'stroke-width': '0', 'fill': '#ddd'});
+	var hourL = paper.path('M 80 80 v -38');
+	hourL.attr({'stroke-width': '1px', 'stroke': '#aaa'});
+
+	// pivot
+	var pivot = paper.circle(center, center, 5);
+	pivot.attr({'stroke-width': 0, fill: '#555'});
+
+	var drawClock = function()
+	{
+		var now = new Date();
+		var m = now.getMinutes() + (now.getSeconds() / 60.0);
+		var h = now.getHours() + (m / 60.0);
+
+		minute.rotate(m * 6, center, center);
+		minuteL.rotate(m * 6, center, center);
+		hour.rotate(h * 30, center, center);
+		hourL.rotate(h * 30, center, center);
+	}
+
+	var setClock = function()
+	{
+		drawClock();
+		setTimeout(function(){setClock()}, 1000);
+	}
+
+	setClock();
 }
